dfb454a3785ec88422488b359dc3c0df56f37f13
Amr_Reda <amrreda547@gmail.com>
2018-10-27 02:18:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ mongoose.connect('mongodb://root:root123@ds141623.mlab.com:41623/to_do_list')
 .then(()=>console.log("mongoDB connected"))
 .catch(err=>console.log(err));
 
+// middleware to accept all apis
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","*");
+  res.header("Access-Control-Allow-Methods","*");
+  next();
+});
+
 app.use('/api',api);
 
 // Serve static assets if in production
@@ -28,23 +36,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000;
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-
-
-
-//middleware to accept all apis
-// app.use((req,res,next)=>{
-//   res.header("Access-Control-Allow-Origin","*");
-//   res.header("Access-Control-Allow-Headers","*");
-//   res.header("Access-Control-Allow-Methods","*");
-//   next();
-// });
-
-
-
-
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
